947a9853590b110f6539082a312448aa66e63ac5
Kevin Kroon <kkroon@student.codam.nl>
2024-02-03 17:59:56 +0100

    with comments from copilot

--- srcs/requirements/mariadb/mariadb.sh
+++ srcs/requirements/mariadb/mariadb.sh
@@ -1,31 +1,46 @@
-#!/bin/bash
+#!/bin/bash # This line tells the system this script should be executed with /bin/bash
 
-if [ ! -e "/var/lib/mysql/.init_complete" ]; then
-  mysql_install_db --user=mysql --datadir=/var/lib/mysql
-  
-  /usr/sbin/mysqld --skip-networking &
+# Check if the MariaDB initialization is complete
+if [ ! -e "/var/lib/mysql/.done" ]; then
+	# Initialize the MariaDB database
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-  for i in {30..0}; do
-      if echo 'SELECT 1' | mysql &> /dev/null; then
-          break
-      fi
-      sleep 1
-  done
+	# Start the MariaDB server without networking
+	/usr/sbin/mysqld --skip-networking &
 
-  if [ "$i" -eq 0 ]; then
-      echo >&2 'MariaDB'
-      exit 1
-  fi
+	# Wait for MariaDB to be ready
+	for i in {30..0}; do
+		# Try to run a simple SQL query
+		if echo 'SELECT 1' | mysql &> /dev/null; then
+			break
+		fi
+		# Wait for 1 second before trying again
+		sleep 1
+	done
 
-  mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
-  mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-  mysql -u root -e "GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';"
-  mysql -u root -e "FLUSH PRIVILEGES;"
-  mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+	# If MariaDB is not ready after 30 seconds, print an error message and exit
+	if [ "$i" -eq 0 ]; then
+		echo >&2 'MariaDB'
+		exit 1
+	fi
 
-  mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+	# Create the database
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
+	# Create the user
+	mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+	# Grant all privileges on the database to the user
+	mysql -u root -e "GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';"
+	# Reload the privilege tables
+	mysql -u root -e "FLUSH PRIVILEGES;"
+	# Set the password for the root user
+	mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
 
-  touch /var/lib/mysql/.init_complete
+	# Shutdown the MariaDB server
+	mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+	# Create a file to indicate that the MariaDB initialization is complete
+	touch /var/lib/mysql/.done
 fi
 
+# Execute the command passed to the script
 exec "$@"
--- srcs/requirements/wordpress/wordpress.sh
+++ srcs/requirements/wordpress/wordpress.sh
@@ -1,13 +1,20 @@
+# This line tells the system this script should be executed with /bin/bash
 #!/bin/bash
 
+# Check if the WordPress configuration file exists
 if [ ! -f ./wp-config.php ]; then
-    wp core download --allow-root
-    wp config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
-    wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
-    wp user create $WP_NORMAL_USER $WP_NORMAL_USER_EMAIL --user_pass=$WP_NORMAL_USER_PASSWORD --role=author --allow-root
+	# Download the WordPress core files
+	wp core download --allow-root
+
+	# Create the WordPress configuration file with the provided database credentials and host
+	wp config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+
+	# Install WordPress with the provided site information and admin credentials
+	wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+	# Create a new WordPress user with the provided credentials and role
+	wp user create $WP_NORMAL_USER $WP_NORMAL_USER_EMAIL --user_pass=$WP_NORMAL_USER_PASSWORD --role=author --allow-root
 fi
-    #wp theme install twentynineteen --activate --allow-root # Install and activate the 'twentynineteen' theme
-	#wp plugin update --all --allow-root # Update all WordPress plugins
 
+# Execute the command passed to the script
 exec "$@"
-
